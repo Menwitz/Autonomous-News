90b098963c1a9d66b7ebe36093dcf5390a8aa8f8
MELGAGIT <gaidimehdi@gmail.com>
2024-07-27 18:14:59 +0300

    Update generate article TU
    Add the time of genetared articles into json file.
    
    input TU file  = tu_in_generate_article.json
    output file     = tu_out_generate_article.json

--- lambdas/generate_article/test_generate_article.py
+++ lambdas/generate_article/test_generate_article.py
@@ -2,18 +2,41 @@
 
 import unittest
 from generate_article_v1 import generate_article
+import json
+import time
 
 class TestOpenAI(unittest.TestCase):
 
-    headlines = [
-        'Swimming star famed for Michael Phelps upset eyes another Olympic gold',
-        'Supreme Court declines to halt former Colorado official trial on charges related to election security breach'
-
-    ]
     def test_generate_article(self):
         #self.assertEqual(generate_article(self.headlines[0]), 0)
-        print(generate_article(self.headlines[0]))
-        print(generate_article(self.headlines[1]))
+            # Load the input file
+        with open('tu_in_generate_article.json', 'r') as file:
+            headlines = json.load(file)
+
+       
+        test_results = []
+        
+        i = 0
+        for test_case in headlines['tu_headlines']:
+            print(test_case)
+            start_time = time.time()
+            output = generate_article(test_case)
+            end_time = time.time()
+
+            count_test_time = end_time - start_time
+
+            test_results.append({
+                "input": test_case,
+                "output": output,
+                "time": count_test_time
+            })
+            i = i + 1
+
+            # Log the results to an output file
+        with open('tu_out_generate_article.json', 'w') as file:
+            json.dump(test_results, file, indent=4)
+
+        print("All test cases logged to tu_out_generate_article.json")
 
 
 if __name__ == '__main__':
